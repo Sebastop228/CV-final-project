316407de077acfb6de9596947e82ccb03ac346e9
Huy Pham <huycan28@gmail.com>
2020-04-22 14:46:08 -0400

    Initial commit

--- code/main.py
+++ code/main.py
@@ -6,7 +6,7 @@ import csv
 import os
 from model import Model
 
-data_dir = '../../../data/' # Change this so it refers to where you have the data
+data_dir = '../../data/' # Change this so it refers to where you have the data
 
 
 def train(model, train_labels, train_images):
