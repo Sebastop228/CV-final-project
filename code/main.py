623ed7c140591f5fb394f90b387247d374c81793
Sebastop228 <49453891+Sebastop228@users.noreply.github.com>
2020-04-26 16:02:43 -0400

    model 2 updating

--- code/main.py
+++ code/main.py
@@ -1,7 +1,8 @@
 import tensorflow as tf 
 import numpy as np
 import hyperparameters as hp
-import pandas as pd  # You're going to need to install this
+import hyperparameters2 as hp2
+# import pandas as pd  # You're going to need to install this
 import csv
 import os
 import argparse
@@ -24,12 +25,12 @@ def parse_args():
 def train(model, train_labels, train_images):
 
     amt_to_train = train_images.shape[0] #Amt of images we're training over
-    batch_size = hp.batch_size # make batch size a hyperparameter, either in model or hyperparameters.py
+    batch_size = hp2.batch_size # make batch size a hyperparameter, either in model or hyperparameters.py
 
     for i in range(0, amt_to_train, batch_size):
         # indexed into the arrays so that, for the last batch, i+batch_size doesn't go over the size of the array
 
-        print("TRAINING: batch ", i, "out of ", amt_to_train)
+        # print("TRAINING: batch ", i, "out of ", amt_to_train)
 
         batch_images = train_images[i:min(i+batch_size, amt_to_train)]
         batch_labels = train_labels[i:min(i+batch_size, amt_to_train)]
@@ -45,7 +46,7 @@ def train(model, train_labels, train_images):
 
 def test(model, test_labels, test_images):
     amt_to_test = test_images.shape[0]
-    batch_size = hp.batch_size
+    batch_size = hp2.batch_size
     amt_correct = 0
     for i in range(0, amt_to_test, batch_size):
         batch_images = test_images[i:min(i+batch_size, amt_to_test)]
@@ -79,8 +80,10 @@ def main():
         else:
             print("Initializing from scratch.")
 
-    for i in range(epoch.numpy(), hp.num_epochs, 1):
+    for i in range(epoch.numpy(), hp2.num_epochs, 1):
+        print("Training for epoch ", i, "out of ", hp2.num_epochs)
         train(model, train_labels, train_images)
+        print("Testing for epoch ", i, "out of ", hp2.num_epochs)
         accuracy = test(model, test_labels, test_images)
         if i % 4 == 3:
             epoch.assign(i + 1)
--- code/model2.py
+++ code/model2.py
@@ -1,6 +1,7 @@
 import tensorflow as tf 
 import numpy as np
 import hyperparameters2 as hp2
+# from tf.keras.layers import Dense, Dropout, Flatten, MaxPool2D, BatchNormalization, Activation
 
 #architecture sourced from http://cs231n.stanford.edu/reports/2016/pdfs/005_Report.pdf
 
@@ -12,63 +13,101 @@ class Model(tf.keras.Model):
         tf.keras.backend.set_floatx('float32')
 
 
-        self.optimizer = tf.keras.optimizers.Adam(learning_rate = hp.learning_rate, decay = hp.decay)
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate = hp2.learning_rate) #, decay = hp2.decay)
         
         #They don't use a kernel initializer in their github code, but we could play around with that.
         #Also for some reason some of the conv layers have same padding and some have valid?
-        self.initializer = tf.keras.initializers.TruncatedNormal(stddev=hp.stddev)
+        self.initializer = tf.keras.initializers.TruncatedNormal(stddev=hp2.stddev)
 
         #self.architecture = tf.keras.Sequential()
-        self.architecture = []
+        self.architecture = [
+
+            # "https://github.com/jrishabh96/Facial-Expression-Recognition/blob/master/cnn_major.py""
+
+
+            tf.keras.layers.Conv2D(64, (3,3), padding = 'same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Activation('relu'),
+            tf.keras.layers.MaxPool2D(pool_size = (2,2)),
+            tf.keras.layers.Dropout(0.25),
+
+            tf.keras.layers.Conv2D(128, (5,5), padding = 'same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Activation('relu'),
+            tf.keras.layers.MaxPool2D(pool_size = (2,2)),
+            tf.keras.layers.Dropout(0.25),
+
+            tf.keras.layers.Conv2D(512, (3,3), padding = 'same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Activation('relu'),
+            tf.keras.layers.MaxPool2D(pool_size = (2,2)),
+            tf.keras.layers.Dropout(0.25),
+
+            tf.keras.layers.Conv2D(512, (3,3), padding = 'same'),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Activation('relu'),
+            tf.keras.layers.MaxPool2D(pool_size = (2,2)),
+            tf.keras.layers.Dropout(0.25),
+
+            tf.keras.layers.Flatten(),
+
+            tf.keras.layers.Dense(512),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Activation('relu'),
+            tf.keras.layers.Dropout(0.25),
+
+            tf.keras.layers.Dense(7, activation = 'softmax') #potentially try softmax?
+
+        ]
     
-        self.architecture.append(tf.keras.layers.Conv2D(filters = 64, kernel_size = 3, strides = (1,1), activation = "relu"))
-        #figure out parameters for batch normalization (I think this is what they mean by
-        # batch normalization)
-        self.architecture.append(tf.keras.layers.BatchNormalization())
-        #figure out dropout rate
-        self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
-        self.architecture.append(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+        # self.architecture.append(tf.keras.layers.Conv2D(filters = 64, kernel_size = 3, strides = (1,1), activation = "relu"))
+        # #figure out parameters for batch normalization (I think this is what they mean by
+        # # batch normalization)
+        # self.architecture.append(tf.keras.layers.BatchNormalization())
+        # #figure out dropout rate
+        # self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
+        # self.architecture.append(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
         
-        self.architecture.append(tf.keras.layers.Conv2D(filters = 128, kernel_size = 5, strides = (1,1), activation = "relu"))
-        #figure out parameters for batch normalization
-        self.architecture.append(tf.keras.layers.BatchNormalization())
-        #figure out dropout rate
-        self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
-        self.architecture.append(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+        # self.architecture.append(tf.keras.layers.Conv2D(filters = 128, kernel_size = 5, strides = (1,1), activation = "relu"))
+        # #figure out parameters for batch normalization
+        # self.architecture.append(tf.keras.layers.BatchNormalization())
+        # #figure out dropout rate
+        # self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
+        # self.architecture.append(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
 
-        self.architecture.append(tf.keras.layers.Conv2D(filters = 512, kernel_size = 3, strides = (1,1), activation = "relu"))
-        #figure out parameters for batch normalization
-        self.architecture.append(tf.keras.layers.BatchNormalization())
-        #figure out dropout rate
-        self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
-        self.architecture.append(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+        # self.architecture.append(tf.keras.layers.Conv2D(filters = 512, kernel_size = 3, strides = (1,1), activation = "relu"))
+        # #figure out parameters for batch normalization
+        # self.architecture.append(tf.keras.layers.BatchNormalization())
+        # #figure out dropout rate
+        # self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
+        # self.architecture.append(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
 
-        self.architecture.append(tf.keras.layers.Conv2D(filters = 512, kernel_size = 3, strides = (1,1), activation = "relu"))
-        #figure out parameters for batch normalization
-        self.architecture.append(tf.keras.layers.BatchNormalization())
-        #figure out dropout rate
-        self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
-        self.architecture.append(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+        # self.architecture.append(tf.keras.layers.Conv2D(filters = 512, kernel_size = 3, strides = (1,1), activation = "relu"))
+        # #figure out parameters for batch normalization
+        # self.architecture.append(tf.keras.layers.BatchNormalization())
+        # #figure out dropout rate
+        # self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
+        # self.architecture.append(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
 
-        self.architecture.append(tf.keras.layers.Flatten())
+        # self.architecture.append(tf.keras.layers.Flatten())
 
-        self.architecture.append(tf.keras.layers.Dense(256, activation='relu'))
-        #figure out parameters for batch normalization
-        self.architecture.append(tf.keras.layers.BatchNormalization())
-        #figure out dropout rate
-        self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
+        # self.architecture.append(tf.keras.layers.Dense(256, activation='relu'))
+        # #figure out parameters for batch normalization
+        # self.architecture.append(tf.keras.layers.BatchNormalization())
+        # #figure out dropout rate
+        # self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
 
-        self.architecture.append(tf.keras.layers.Flatten())
+        # self.architecture.append(tf.keras.layers.Flatten())
 
-        self.architecture.append(tf.keras.layers.Dense(512, activation='relu'))
-        #figure out parameters for batch normalization
-        self.architecture.append(tf.keras.layers.BatchNormalization())
-        #figure out dropout rate
-        self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
+        # self.architecture.append(tf.keras.layers.Dense(512, activation='relu'))
+        # #figure out parameters for batch normalization
+        # self.architecture.append(tf.keras.layers.BatchNormalization())
+        # #figure out dropout rate
+        # self.architecture.append(tf.keras.layers.Dropout(rate = 0.25))
 
-        self.architecture.append(tf.keras.layers.Flatten())
+        # self.architecture.append(tf.keras.layers.Flatten())
 
-        self.architecture.append(tf.keras.layers.Dense(7, activation = "softmax"))
+        # self.architecture.append(tf.keras.layers.Dense(7, activation = "softmax"))
 
 
     def call(self, inputs):
@@ -85,7 +124,7 @@ class Model(tf.keras.Model):
 
         # Added one-hot encoding in order to compute loss
         one_hot = tf.keras.utils.to_categorical(labels)
-        return tf.keras.losses.categorical_crossentropy(one_hot, predictions, from_logits = False)
+        return tf.keras.losses.categorical_crossentropy(one_hot, predictions, from_logits = False) #try binary crossentropy?
 
 
     def accuracy_fn(self, labels, probs):
@@ -95,10 +134,10 @@ class Model(tf.keras.Model):
         #the number of "correct" predictions; that is, the amount of images (of which we have batch_size) for which the highest probability
         #is the correct emotion (which we would get from looking at the corresponding label)
         
-        print("LABEL IS ", labels)
-        print("LABEL SHAPE IS ", labels.shape)
+        # print("LABEL IS ", labels)
+        # print("LABEL SHAPE IS ", labels.shape)
         highest_prediction_index = np.argmax(probs, axis = 1)
         amt_correct = np.count_nonzero(labels == highest_prediction_index)
-        print("AMT CORRECT IS ", amt_correct)
+        # print("AMT CORRECT IS ", amt_correct)
         #exit(0)
         return amt_correct
