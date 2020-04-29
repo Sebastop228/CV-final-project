788557e791a3ff0ce55d2d8ac6ba8de0ad3ca1c5
nastassiagoodson <60201494+nastassiagoodson@users.noreply.github.com>
2020-04-29 13:51:29 -0700

    h

--- code/main.py
+++ code/main.py
@@ -146,8 +146,8 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    cap.release()
-    cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
