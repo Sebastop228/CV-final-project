4527bbee4b18d9c96851328f046048e9054fded2
monicaroy <monica_roy@brown.edu>
2020-05-01 13:51:35 -0400

    removed last print statement

--- code/main.py
+++ code/main.py
@@ -157,8 +157,7 @@ def main():
             train(augment, model, train_labels, train_images, validation_data)
 
             #results = model.evaluate(test_images, test_labels, batch_size=model.batch_size)
-            results = test(model, test_labels, test_images)
-            print('test loss, test acc:', results)
+            test(model, test_labels, test_images)
 
         else:
 
@@ -183,8 +182,7 @@ def main():
             train(augment, model, train_labels, train_images, validation_data)
 
             #results = model.evaluate(test_images, test_labels, batch_size=model.batch_size)
-            results = test(model, test_labels, test_images)
-            print('test loss, test acc:', results)
+            test(model, test_labels, test_images)
             
 
         model.save_weights('model.h5')
